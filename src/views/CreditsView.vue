8d4d07f53133feabeca64a65998ab5d03ac15546
Aurelienporte <contact.aurelien.dev@gmail.com>
2026-02-19 16:08:00 +0100

    ||Data|| add content to dataProject. ||VIEWS|| correct typo and improve hyphenation in CreditsView

--- src/views/CreditsView.vue
+++ src/views/CreditsView.vue
@@ -9,18 +9,18 @@ import TheFooter from '@/components/TheFooter.vue';
   <MainScrollX>
     <InfosWrapper title="Technologies">
       <div class="credits__container">
-        <h3 class="credits__subtitle">Web app</h3>
+        <h3 class="credits__subtitle">Ceci n'est pas un site</h3>
         <p class="credits__text">
-            Ce site est une Progressive Web Application (PWA) réalisée en JavaScript, html et css avec Vue.js.
-            Il est par conséquent possible de le télécharger et de le consulter comme une application native.
+            C'est une Progressive Web Appli&shy;ca&shy;tion (PWA) réalisée en&nbsp;Java&shy;Script, HTML et&nbsp;CSS avec Vue.js.
+            Il&nbsp;est par con&shy;sé&shy;quent possible de le télé&shy;char&shy;ger et de le con&shy;sul&shy;ter comme une application native.
         </p>
         <h3 class="credits__subtitle">Vue.js</h3>
         <p class="credits__text">
-            Un framework open source imaginé et maintenu par Evan You. Il permet de construire des 
-            interfaces utilisateur réactives et performantes, en adoptant une approche déclarative et basée sur les composants.</p>
+            Est un framework open source imaginé et&nbsp;main&shy;tenu par Evan You. Il&nbsp;permet de&nbsp;construire des 
+            inter&shy;fa&shy;ces utilisateur réacti&shy;ves et&nbsp;perfor&shy;mantes, en adoptant une&nbsp;approche déclarati&shy;ve et&nbsp;basée sur les&nbsp;composants.</p>
         <h3 class="credits__subtitle">Vue Router</h3>
         <p class="credits__text">
-            Développé par Eduardo San Martin Morote et Evan You, assure la navigation programmatique entre les différentes pages du site.  
+            Développé par Eduardo San Martin Morote et Evan You, assure la&nbsp;navi&shy;ga&shy;tion programmatique entre les&nbsp;diffé&shy;rentes pages du site.  
         </p>
       </div>
     </InfosWrapper>
@@ -28,17 +28,17 @@ import TheFooter from '@/components/TheFooter.vue';
       <div class="credits__container">
         <h3 class="credits__subtitle">Les fontes utilisées</h3>
         <p class="credits__text">
-            Sont disponibles gratuitement sur Google Fonts, une bibliothèque de polices de caractères 
-            libres de droits.
+            Sont disponibles gratuitement sur Google Fonts, une bibliothèque de&nbsp;poli&shy;ces de caractères 
+            libres de&nbsp;droits.
         </p>
         <h3 class="credits__subtitle">Exo 2</h3>
         <p class="credits__text">
-            Ici employée pour les textes et sous-titres, est dessinée par Natanael Gama.
-            C'est une fonte géométrique, contemporaine, sans sérif au design futuriste.</p>
+            Ici employée pour les textes et&nbsp;sous-titres, est&nbsp;dessi&shy;née par Natanael Gama.
+            C'est une fonte géométrique, contem&shy;poraine, sans sérif au design futuriste.</p>
         <h3 class="credits__subtitle">Questrial</h3>
         <p class="credits__text">
             Ici employée pour les titres, a été créée par Laura Meseguer et Joe Prince.
-            C'est une fonte sans sérif avec des formes circulaires inspirées des grostesques suisses comme Helvetica. 
+            C'est une fonte sans sérif avec des formes circulai&shy;res inspirées des grostesques suisses comme Helvetica. 
         </p>
       </div>
     </InfosWrapper>
@@ -46,17 +46,17 @@ import TheFooter from '@/components/TheFooter.vue';
       <div class="credits__container">
         <h3 class="credits__subtitle">Bluesky et Pixelfed</h3>
         <p class="credits__text">
-            Sont des plateformes sociales décentralisées qui mettent l'accent sur la confidentialité, la 
+            Sont des plateformes sociales décen&shy;tra&shy;lisées qui mettent l'accent sur la confi&shy;dentialité, la 
             liberté d'expression et le contrôle des données personnelles.
         </p>
         <p class="credits__text">
-            Elles sans publicité ni algorithmes de recommandation, et n'exigent pas la 
-            création d'un compte pour consulter les contenus. Elles ne financent pas les politiques autoritaires et réactionnaires.
+            Elles sont sans publicité ni algo&shy;rith&shy;mes de recomman&shy;dation, et n'exi&shy;gent pas la&nbsp;création
+             d'un compte pour consulter les conte&shy;nus. Elles ne financent pas l'extrè&shy;me droite ni ne portent atteinte à&nbsp;la démocratie.
         </p>
         <h3 class="credits__subtitle">Bluesky</h3>
-        <p class="credits__text">A été crée par Jack Dorsey et les équipes de Twitter. Jay Graber en assure la direction.</p>
+        <p class="credits__text">A été crée par Jack Dorsey et les équipes de Twitter. Jay&nbsp;Graber en assure la direction.</p>
         <h3 class="credits__subtitle">Pixelfed</h3>
-        <p class="credits__text">Alternative éthique à Instagram, interopérable sur le Fediverse, créée par Daniel Supernault.</p>
+        <p class="credits__text">Est une alternative éthique à Insta&shy;gram, interopérable sur le Fedi&shy;verse, créée par Daniel Supernault.</p>
       </div>
     </InfosWrapper>
   </MainScrollX>
